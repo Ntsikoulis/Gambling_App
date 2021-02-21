260f2db14c623643a357c8f6d719546c0dad8dc3
Nickos <72402710+dai19281@users.noreply.github.com>
2021-02-21 17:38:12 +0200

    Updates

---
+++ precache-manifest.6bce1a55cf242f98fc2725ec26e6be04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a51a7eef1a6770c4e2f4c5844e4c3eed",
+    "url": "/Blockchain_Contract/index.html"
+  },
+  {
+    "revision": "bcc7650706ce061a4802",
+    "url": "/Blockchain_Contract/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "4242b7223a6f5c281762",
+    "url": "/Blockchain_Contract/static/js/2.7fbb6e1d.chunk.js"
+  },
+  {
+    "revision": "bcc7650706ce061a4802",
+    "url": "/Blockchain_Contract/static/js/main.7a2eee8a.chunk.js"
+  },
+  {
+    "revision": "8488f53b175b9c566798",
+    "url": "/Blockchain_Contract/static/js/runtime-main.307eb0f2.js"
+  }
+]);
